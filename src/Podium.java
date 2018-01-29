3e636aae32ca4c987013f1141f043e725e364a9c
Dylan <Champeme.dylan@gmail.com>
2018-01-29 16:57:18 +0100

    Maj Podium

--- src/Podium.java
+++ src/Podium.java
@@ -18,35 +18,54 @@ public class Podium {
         podium.setBackground(Color.DARK_GRAY);
         // Police et taille du titre
         Font f = new Font("Arial", Font.PLAIN, 36);
-        // ??????
+        // Défini la couleur de la police
         podium.setForeground(Color.BLUE);
         // Definition de la police du panel
         podium.setFont(f);
 
-        //Création d'une fenetre
+        // Création d'une fenetre
         JWindow fenPodium = new JWindow();
-        //On lui donne une taille pour qu'on puisse la voir
-        fenPodium.setSize(500, 500);
+        // On lui donne une taille pour qu'on puisse la voir
+        fenPodium.setSize(800, 800);
         // Centre la fenetre au milieu de l'écran
         fenPodium.setLocationRelativeTo(null);
         // Définition du content pane
         fenPodium.setContentPane(podium);
+        // Garder au premier plan
+        fenPodium.setAlwaysOnTop(true);
 
         //Rendre podium visible
         fenPodium.setVisible(true);
 
-        //Bouton lancerPartie
-        podium.add(retourMenu);
+        //On définit le layout à utiliser sur le content pane
+        fenPodium.setLayout(new BorderLayout());
 
-        //Bouton de sortie "Exit"
-        podium.add(exit);
 
+
+
+        // Haut de Page
+        fenPodium.getContentPane().add(new JLabel("Scores"), BorderLayout.NORTH);
+        // Au centre
+        fenPodium.getContentPane().add(new JTable(15, 3), BorderLayout.CENTER);
+        // Bouton lancerPartie
+        podium.add(retourMenu, BorderLayout.SOUTH);
+        // Bouton de sortie "Exit"
+        podium.add(exit, BorderLayout.SOUTH);
+
+
+
+
+        // rendre la fenetre visible
+        fenPodium.setVisible(true);
+
+        // Event bouton exit
         exit.addActionListener(actionEvent -> {
             System.out.println("Click ! btn exit");
             // Quitter le programme
             System.exit(0);
         });
 
+        // Event bouton retourMenu
         retourMenu.addActionListener(actionEvent -> {
             System.out.println("Click ! btn Menu");
             // Fermer la fenetre du podium
