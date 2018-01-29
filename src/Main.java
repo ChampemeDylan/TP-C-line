8ca1c51849815e26fd3de618207a1c56449a6d32
mikerhinos <metromg1>
2018-01-29 16:56:48 +0100

    Ajout d'un gain au parieur, ajout d'une classe de gestion des paris. TODO : méthode de calcul du gain et répartition aux gagnants.

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         Accueil accueil = new Accueil();
         accueil.setVisible(true);
+        Paris paris = new Paris();
     }
 }
