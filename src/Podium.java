00f45a0c8fed7024046f552748fd1b2989a6cae8
Dylan <Champeme.dylan@gmail.com>
2018-01-29 12:30:08 +0100

    Maj Podium

--- src/Podium.java
+++ src/Podium.java
@@ -1,19 +1,47 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class Podium {
+
+    JButton retourMenu = new JButton("Menu principal");
+    JButton exit = new JButton("Exit");
 
     public Podium() {
         fenetre();
     }
 
     public void fenetre() {
-        JWindow podium = new JWindow();
+
+        // Création du panel
+        JPanel podium = new JPanel();
+        // couleur de fond du panel
+        podium.setBackground(Color.DARK_GRAY);
+        // Police et taille du titre
+        Font f = new Font("Arial", Font.PLAIN, 36);
+        // ??????
+        podium.setForeground(Color.BLUE);
+        // Definition de la police du panel
+        podium.setFont(f);
+
+        //Création d'une fenetre
+        JWindow fenPodium = new JWindow();
         //On lui donne une taille pour qu'on puisse la voir
-        podium.setSize(500, 500);
-        //On la rend visible
-        podium.setVisible(true);
+        fenPodium.setSize(500, 500);
         // Centre la fenetre au milieu de l'écran
-        podium.setLocationRelativeTo(null);
+        fenPodium.setLocationRelativeTo(null);
+        // Définition du content pane
+        fenPodium.setContentPane(podium);
+
+        //Rendre podium visible
+        fenPodium.setVisible(true);
+
+        //Bouton lancerPartie
+        podium.add(Menu);
+        retourMenu.addActionListener();
+
+        //Bouton de sortie "Exit"
+        podium.add(exit);
+
     }
 
     private void mettreEnOrdre() {
