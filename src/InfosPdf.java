d5db2d2ae2a32e2787e73a793f86ed31e8ecabac
cypherpeon <31858939+cypherpeon@users.noreply.github.com>
2018-02-02 15:07:45 +0100

    maj InfosPdf de Malika

--- src/InfosPdf.java
+++ src/InfosPdf.java
@@ -6,6 +6,7 @@
 
 import java.io.FileOutputStream;
 import java.io.IOException;
+
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Paragraph;
@@ -21,17 +22,19 @@ public class InfosPdf {
 
     /**
      * Création du fichier : Informations.pdf
-     * @param    args   pas besoin d'arguments
+     *
+     * @param args pas besoin d'arguments
      */
-    public static void main(String[] args)throws DocumentException, IOException {
+    public static void main(String[] args) throws DocumentException, IOException {
         new InfosPdf().creattionPdf(PDF);
     }
 
     /**
      * Création du document PDF.
+     *
      * @param nomExtension le conteneur.
-     * @throws    DocumentException
-     * @throws    IOException
+     * @throws DocumentException
+     * @throws IOException
      */
     public void creattionPdf(String nomExtension)
             throws DocumentException, IOException {
@@ -50,10 +53,27 @@ public class InfosPdf {
         /**
          * Etaê 4 : Ajout des différents contenus.
          */
-        document.add(new Paragraph("Contenu 1"));
-        document.add(new Paragraph("Contenu 2!"));
-        document.add(new Paragraph("Contenu 3!"));
-        document.add(new Paragraph("Contenu 4!"));
+/**
+ * Etape 4 : Ajout des différents contenus.
+ */
+        Personne personne = new Personne("Laraba", "Anis", 37, "masculin");
+        Palmares palmares = new Palmares(1, 3);
+        Pilote pilote = new Pilote(personne.getNom(), personne.getPrenom(), personne.getAge(), personne.getSexe(), 1, palmares, 10);
+        Vehicule vehicule = new Vehicule(1, 100, pilote, "bmw", "pro");
+        Voiture voiture = new Voiture(vehicule.getNumero(), vehicule.getPuissance(), pilote, vehicule.getMarque(), vehicule.getCategorie());
+        Ecurie ecurie = new Ecurie("Mercedes", "Allemande", vehicule, pilote);
+
+
+        document.add(new Paragraph("BIENVENUE SUR LA COURSE"));
+        document.add(new Paragraph("Le nom du joueur 1 : " + personne.getNom()));
+        document.add(new Paragraph("Le prenom du joueur 1 : " + personne.getPrenom()));
+        document.add(new Paragraph("Votre âge : " + personne.getAge()));
+        document.add(new Paragraph("Votre sexe : " + personne.getSexe()));
+        document.add(new Paragraph("Vous avez la voiture numéro : " + vehicule.getNumero()));
+        document.add(new Paragraph("Votre voiture est une : " + vehicule.getMarque()));
+        document.add(new Paragraph("Votre voiture a une puissance de : " + vehicule.getPuissance()));
+        document.add(new Paragraph("Votre voiture est de la catégorie : " + vehicule.getCategorie()));
+        document.add(new Paragraph("Vous faites partie de l'écurie : " + ecurie.getNom() + " de nationalité " + ecurie.getNationalite()));
         /**
          * Etape 5 : Fermeture du documents PDF. (Sauvegarde).
          */
